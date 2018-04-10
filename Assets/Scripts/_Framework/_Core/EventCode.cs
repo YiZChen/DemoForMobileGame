6c9f6a5937ff71700794d324b1edbf906b329b01
YIZChen <cyz8889@sina.cn>
2018-04-10 11:41:21 +0800

    Only remain prefab folder to the resources

---
+++ Assets/Scripts/_Framework/_Core/EventCode.cs
@@ -0,0 +1,33 @@
+﻿//游戏事件码定义规则
+//0-20 模块码 | 21-30 成就码 | 31-40 声音码 | 41-50 商店码
+//| 51-80 UI码 | 81-90 输入码 | 90-120 游戏逻辑码
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCode
+{
+    #region Achievement事件集合
+
+    #endregion
+
+    #region Audio事件集合
+
+    #endregion
+
+    #region Shop事件集合
+
+    #endregion
+
+    #region UI事件集合
+
+    #endregion
+
+    #region Input事件集合
+
+    #endregion
+
+    #region Game事件集合
+
+    #endregion
+}
